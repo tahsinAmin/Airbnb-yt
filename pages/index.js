1d9ea144a3092341e47f83aa9d8a64d861825625
tahsinAmin <muhammadtahsinamin@gmail.com>
2021-08-29 19:17:03 +0600

    Finishing of small card component

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,10 @@
 import Head from 'next/head'
 import Header from '../components/Header'
 import Banner from '../components/Banner'
+import SmallCard from '../components/SmallCard';
+import MediumCard from '../components/MediumCard';
 
-export default function Home({ exploreData }) {
+export default function Home({ exploreData, cardData }) {
   return (
     <div className="">
       <Head>
@@ -16,9 +18,28 @@ export default function Home({ exploreData }) {
         <section className='pt-6'>
           <h2 className='text-4xl font-semibold pb-5'>Explore Nearby</h2>
           {/* Pul some data from a server - API  */}
-          {exploreData?.map((item) => (
-            <h1>{item.location}</h1>
+          <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
+          {exploreData?.map(({img, location, distance}) => (
+            <SmallCard
+             key={img} 
+             img={img} 
+             distance={distance} 
+             location={location}/>
           ))}
+          </div>
+        </section>
+        <section>
+          <h2 className='text-4xl font-semibold py-8'>Live Anywhere</h2>
+          
+          <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
+          {cardData?.map(({img, location, distance}) => (
+            <MediumCard
+            key={img} 
+            img={img} 
+            distance={distance} 
+            location={location}/>
+          ))}
+          </div>
         </section>
       </main>
     </div>
@@ -30,9 +51,14 @@ export async function getStaticProps(){ //whatever we use hinside here is happen
     then(res => res.json()
   );
 
+  const cardData = await fetch("https://links.papareact.com/zp1").
+    then(res => res.json()
+  );
+
   return {
     props: {
-      exploreData
+      exploreData,
+      cardData
     }
   }
 }
