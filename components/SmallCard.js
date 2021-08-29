9ffe72f929c76ce970844e71533a500b7fa5c5dc
tahsinAmin <muhammadtahsinamin@gmail.com>
2021-08-29 08:49:52 +0600

    Server side static page render. This is for bringing json data

---
+++ components/SmallCard.js
@@ -0,0 +1,9 @@
+function SmallCard() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default SmallCard
--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import Header from '../components/Header'
 import Banner from '../components/Banner'
 
-export default function Home() {
+export default function Home({ exploreData }) {
   return (
     <div className="">
       <Head>
@@ -11,8 +11,28 @@ export default function Home() {
       </Head>
       <Header/>
       <Banner/>
-      
 
+      <main className='max-w-7xl mx-auto px-8 sm:px-16'>
+        <section className='pt-6'>
+          <h2 className='text-4xl font-semibold pb-5'>Explore Nearby</h2>
+          {/* Pul some data from a server - API  */}
+          {exploreData?.map((item) => (
+            <h1>{item.location}</h1>
+          ))}
+        </section>
+      </main>
     </div>
-  )
+  );
+}
+
+export async function getStaticProps(){ //whatever we use hinside here is happening on the server.
+  const exploreData = await fetch("https://links.papareact.com/pyp").
+    then(res => res.json()
+  );
+
+  return {
+    props: {
+      exploreData
+    }
+  }
 }
