1deaaf6b3dde1f3ca8b5675172e41525233d20be
tahsinAmin <muhammadtahsinamin@gmail.com>
2021-08-30 09:42:29 +0600

    Footer created

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,9 @@ import Header from '../components/Header'
 import Banner from '../components/Banner'
 import SmallCard from '../components/SmallCard';
 import MediumCard from '../components/MediumCard';
+import LargeCard from '../components/LargeCard';
+import Footer from '../components/Footer';
+
 
 export default function Home({ exploreData, cardData }) {
   return (
@@ -31,17 +34,24 @@ export default function Home({ exploreData, cardData }) {
         <section>
           <h2 className='text-4xl font-semibold py-8'>Live Anywhere</h2>
           
-          <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
-          {cardData?.map(({img, location, distance}) => (
+          <div className='flex space-x-3 overflow-scroll scrollbar-hide p-3 ml-3'>
+          {cardData?.map(({img, title}) => (
             <MediumCard
             key={img} 
             img={img} 
-            distance={distance} 
-            location={location}/>
+            title={title}/>
           ))}
           </div>
         </section>
+        <LargeCard
+          img='https://links.papareact.com/4cj'
+          title='The Greatest Outdoors'
+          description='Whishlists curated byb Airbnb'
+          buttonText='Get Inspired'
+          
+        />
       </main>
+      <Footer/>
     </div>
   );
 }
