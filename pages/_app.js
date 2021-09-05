6490be3af7f59f3ddec5ef2a7b5ce948968d5b23
tahsinAmin <muhammadtahsinamin@gmail.com>
2021-09-05 17:23:02 +0600

    Implementing the loading bar

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,18 @@
 import 'tailwindcss/tailwind.css'
 import '../styles/global.css'
+import Router from 'next/router';
+import ProgressBar from "@badrap/bar-of-progress"; // https://www.npmjs.com/package/@badrap/bar-of-progress/v/0.1.1?activeTab=dependents
+ 
+const progress = new ProgressBar({
+  size: 4,
+  color: '#FE595E',
+  className: 'z-50',
+  delay: 100,
+});
+
+Router.events.on('routeChangeStart', progress.start)
+Router.events.on('routeChangeComplete', progress.finish)
+Router.events.on('routeChangeError', progress.finish)
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
